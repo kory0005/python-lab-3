34f71a6aafacede4df574a051ff9457e4a746b4c
kory0005 <kory0005@algonquinlive.com>
2019-09-23 02:36:31 -0500

    adding task-10.py to the repository. The file is about MPG for a car

---
+++ task-10.py
@@ -0,0 +1,4 @@
+miles = input('Enter the number of miles driven: ')
+gallons = input('Enter the number of gallons used: ')
+mpg = int(miles) / int(gallons)
+print('Your car consumption is equal to ' + str(mpg))
